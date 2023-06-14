752304a4082f3d2fe35587f8887342dd35b5588c
M1nseoPark <kjms0820@gmail.com>
2023-06-14 15:36:52 +0900

    js 파일 분리

---
+++ src/main/resources/static/js/itemForm.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    var errorMessage = [[${errorMessage}]];
+    if(errorMessage != null){
+        alert(errorMessage);
+    }
+
+    bindDomEvent();
+});
+
+function bindDomEvent(){
+    $(".custom-file-input").on("change", function() {
+        var fileName = $(this).val().split("\\").pop();  //이미지 파일명
+        var fileExt = fileName.substring(fileName.lastIndexOf(".")+1); // 확장자 추출
+        fileExt = fileExt.toLowerCase(); //소문자 변환
+
+        if(fileExt != "jpg" && fileExt != "jpeg" && fileExt != "gif" && fileExt != "png" && fileExt != "bmp"){
+            alert("이미지 파일만 등록이 가능합니다.");
+            return;
+        }
+
+        $(this).siblings(".custom-file-label").html(fileName);
+    });
+}
+
+function showPopup(multipleFilter) {
+    const popup = document.querySelector('#popup');
+
+    if (multipleFilter) {
+        popup.classList.add('multiple-filter');
+    } else {
+        popup.classList.remove('multiple-filter');
+    }
+
+    popup.classList.remove('hide');
+}
+
+function pickPopup(obj) {
+    const popup = document.querySelector('#popup');
+    var result = document.getElementById("result");
+    var text = obj.innerHTML;
+
+    result.value = text
+    popup.classList.add('hide');
+}
